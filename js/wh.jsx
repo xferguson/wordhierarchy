211d8c32c610f33b72799be8fa02fec6b221a086
Alexander Ferguson <x@echonotation.com>
2017-07-22 09:01:35 +0800

    Add primary tier terms with Reactjs

--- js/wh.jsx
+++ js/wh.jsx
@@ -1,20 +1,24 @@
 var buildTable = function(data, target, termType) {
 	var termType = undefined === termType ? 'Term' : termType;
 	var WHTable = function(props) {
+		var rows = props.data;
+		const tableRows = rows.map((row) =>
+			<div className='wh-row'>
+				<div className='wh-cell wh-primary'><p>{row.word}</p></div>
+				<div className='wh-cell wh-secondary'><p>test vocab</p></div>
+				<div className='wh-cell wh-tertiary'><p>test vocab</p></div>
+			</div>
+		);
 		return (
 			<div className='wh-table'>
 				<div className='wh-row wh-header-row'>
-					<div className='wh-cell'><h3>Primary {props.term}</h3></div>
-					<div className='wh-cell'><h3>Secondary {props.term}</h3></div>
-					<div className='wh-cell'><h3>Tertiary {props.term}</h3></div>
+					<div className='wh-cell wh-primary'><h3>Primary {props.term}</h3></div>
+					<div className='wh-cell wh-secondary'><h3>Secondary {props.term}</h3></div>
+					<div className='wh-cell wh-tertiary'><h3>Tertiary {props.term}</h3></div>
 				</div>
-				<div className='wh-row'>
-					<div className='wh-cell'><p>test vocab</p></div>
-					<div className='wh-cell'><p>test vocab</p></div>
-					<div className='wh-cell'><p>test vocab</p></div>
-				</div>
+				{tableRows}
 			</div>
-		)
+		);
 	};
 
 	ReactDOM.render(
@@ -23,4 +27,4 @@ var buildTable = function(data, target, termType) {
 	);
 };
 
-buildTable(sampleHierarchy, 'wh-sample');
+buildTable(sampleHierarchy, 'wh-sample', "Emotion");

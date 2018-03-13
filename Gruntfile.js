9e6df521d2947058b8ea03ff10d21fede68c19f0
Alexander Ferguson <x@echonotation.com>
2018-03-13 17:40:48 -0600

    Add eslint-plugin-react fo correct react and jsx linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
+/* eslint-env node */
 module.exports = function(grunt) {
-	require('load-grunt-tasks')(grunt);
+	require("load-grunt-tasks")(grunt);
 
 	// Project configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 		eslint: {
-			target: ['Gruntfile.js', 'js/wh.jsx']
+			target: ["Gruntfile.js", "js/wh.jsx", "js/sample-data.js"]
 		}
 	});
 
-	grunt.registerTask('default', ['eslint']);
+	grunt.registerTask("default", ["eslint"]);
 };
